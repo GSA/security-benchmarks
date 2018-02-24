6df700ba53deb98489f4e574251a5b77d1ef1186
Aidan Feldman <aidan.feldman@gsa.gov>
2018-02-24 12:08:57 -0500

    use a table for scp testing

--- scp/test.py
+++ scp/test.py
@@ -4,9 +4,14 @@ import unittest
 
 class TestSCPGenerator(unittest.TestCase):
     def test_is_approved(self):
-        self.assertTrue(is_approved({'Approval Status': 'Approved'}))
-        self.assertTrue(is_approved({'Approval Status': 'approved'}))
-        self.assertFalse(is_approved({'Approval Status': 'nope'}))
+        tests = {
+            'Approved': True,
+            'approved': True,
+            'nope': False
+        }
+        for status, expected in tests.items():
+            actual = is_approved({'Approval Status': status})
+            self.assertEqual(actual, expected, "{} should have resulted in is_approved()=={}".format(status, expected))
 
 
 if __name__ == '__main__':
